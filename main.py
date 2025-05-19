70f5ec4a37c1eb375a103e4e82c61c377badd242
knoxyys <stirlingthebean@gmail.com>
2025-05-19 14:35:36 +1000

    ending on a low (fix later)

--- main.py
+++ main.py
@@ -26,17 +26,26 @@ class Student(Person):
     def showclasses(self):
         return [subject.subject_name for subject in self.subjects] # fix :)
 
+class Teacher(Person):
+    def __init__(self, fname, lname, subject_teach):
+        self.subject_teach = subject_teach
+        super().__init__(fname, lname)
+    
+    def get_info(self):
+        return self.fname, self.lname, self.subject_teach
 
 class Subject:
     def __init__(self, subject_name):
         self.subject_name = subject_name
         self.students = [] # for the student ids
-    
     def print_student_list(self):
         return print(self.students)
     
     def enrol_student(self, student_id):
         self.students.append(student_id)
+    
+    def print_info(self):
+        return print(f"Subject: {self.subject_name}, Students: {self.students}, Teacher: {self.subject_name.teacher.fname} {self.subject_name.teacher.lname}")
 
 
 
@@ -50,6 +59,9 @@ music = Subject("Music 1")
 physics = Subject("Physics")
 sor = Subject("Studies of Religion 1")
 
+mrstor = Teacher("Sarah", "Tor", "Software Engineering")
+print(mrstor.get_info())
+
 stirling = Student("Stirling", "Knox", "100125", "Broughton")
 stirling.enrol_class(mathsext)
 stirling.enrol_class(mathsadv)
@@ -62,3 +74,5 @@ stirling.enrol_class(sor)
 print(stirling.showclasses())
 print(stirling.printname())
 mathsext.print_student_list()
+software.print_student_list()
+software.print_info()
