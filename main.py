b83de023c77c3201da0481bfe796dae3e80ff7dc
knoxyys <stirlingthebean@gmail.com>
2025-05-19 14:14:20 +1000

    mandatory commit #2

--- main.py
+++ main.py
@@ -23,26 +23,42 @@ class Student(Person):
         self.subjects.append(subject_name)
         subject_name.enrol_student(self.student_id)
     
-    def show_classes(self):
-        return self.subjects
+    def __str__(self):
+        subject_names = [subject.subject_name for subject in self.subjects]
+        return ', '.join(subject_names) # i cant lie i got the first line from chatgpt BUT i did know what to do
+
 
 class Subject:
     def __init__(self, subject_name):
         self.subject_name = subject_name
         self.students = [] # for the student ids
     
-    def student_list(self):
-        return self.students
+    def print_student_list(self):
+        return print(self.students)
     
     def enrol_student(self, student_id):
         self.students.append(student_id)
 
-maths = Subject("Maths")
-english = Subject("English")
-software = Subject("Software Engineering")
 
-lucas = Student("Lucas", "Yang", "ID123987", "Marsh")
-lucas.enrol_class(maths)
-lucas.enrol_class(english)
-print(lucas.printname())
-print(maths.student_list())
+# the main line is here
+
+mathsext = Subject("Maths Extension 1")
+mathsadv = Subject("Maths Advanced")
+englishadv = Subject("English Advanced")
+software = Subject("Software Engineering")
+music = Subject("Music 1")
+physics = Subject("Physics")
+sor = Subject("Studies of Religion 1")
+
+stirling = Student("Stirling", "Knox", "100125", "Broughton")
+stirling.enrol_class(mathsext)
+stirling.enrol_class(mathsadv)
+stirling.enrol_class(englishadv)
+stirling.enrol_class(software)
+stirling.enrol_class(music)
+stirling.enrol_class(physics)
+stirling.enrol_class(sor)
+
+print(stirling)
+print(stirling.printname())
+mathsext.print_student_list()
